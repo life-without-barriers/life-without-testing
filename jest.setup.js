4af297270e3b847d52cf502b8bb0a42287d036ea
ethan <ethan.williams@lwb.org.au>
2017-10-06 16:08:37 +1100

    add react setup warning

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,15 @@
-window.React = require('react')
-const { configure } = require('enzyme')
-const Adapter = require('enzyme-adapter-react-16')
+let ok = true
+try {
+  window.React = require('react')
+  const { configure } = require('enzyme')
+  const Adapter = require('enzyme-adapter-react-16')
 
-configure({ adapter: new Adapter() })
+  configure({ adapter: new Adapter() })
+} catch (e) {
+  console.warn('Doesn\'t look like this is a React app, or there are some missing/misconfigured packages.\n\nSkipping React setup...')
+  ok = false
+}
+
+if (ok) {
+  console.log('Bootstrapping React tests...')
+}
